264035f4275b8f531b9bcea05cf0e68c7a7cba81
ldc-37 <ldc791551236@gmail.com>
2018-02-28 17:49:01 +0800

    finish login,join/leave broadcast

--- 4Chatroom/app.js
+++ 4Chatroom/app.js
@@ -9,16 +9,25 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
+	var username = 'unnamed';
 	userOnline++;
-	console.log('a user connected.now online ' + userOnline);
-	socket.on('disconnect', function(){
-		userOnline--;
-		console.log('user disconnected.now online ' + userOnline);
+	console.log('a user connected. Online ' + userOnline);
+
+	socket.on('login',function(data){
+		username = data;
+		console.log(data + ' login');
+		io.emit('userLogin', username);
 	});
 	
 	socket.on('message', function(data){
-		console.log('send message: ' + data);
+		console.log(username + ' send message: ' + data);
 		io.emit('broadcast', data);
+	});
+
+	socket.on('disconnect', function(){
+		userOnline--;
+		console.log('User ' + username +' disconnected. Online ' + userOnline);
+		io.emit('userLogout', username);
 	});
 });
 
